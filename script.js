a82fd94fd70dba3259340291e81a22b8d31750c8
metlandus <metlandus@hotmail.com.tr>
2024-08-24 21:46:42 +0300

    Filter func added

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const itemList = document.querySelector('#item-list');
 const notyf = new Notyf();
 const clearBtn = document.querySelector('#clear');
 const filter = document.querySelector('.filter');
+const swalModal = document.querySelector('.swal-modal');
+
 
 function addItem(e) {
     e.preventDefault();
@@ -18,7 +20,6 @@ function addItem(e) {
 
     const button = createButton("remove-item btn-link text-red");
     li.appendChild(button);
-    console.log(li);
 
     itemList.appendChild(li);
 
@@ -44,14 +45,22 @@ function createIcon(classes) {
 function removeItem(e) {
     if (e.target.parentElement.classList.contains('remove-item')) {
         e.target.parentElement.parentElement.remove();
-        updateClearButtonVisibility();
     }
+    updateClearButtonVisibility();
 }
 
+
 function clearItems() {
-    itemList.innerHTML = '';
-    notyf.success('All items removed', "", "success");
-    updateClearButtonVisibility();
+    swal({
+        text: "Are you sure you want to remove all items?",
+        buttons: {confirm: "Yes", cancel: "No"},
+    }).then((willDelete) => {
+        if (willDelete) {
+            itemList.innerHTML = '';
+            notyf.success('All items removed');
+            updateClearButtonVisibility();
+        }
+    });
 }
 
 function countItems() {
@@ -68,10 +77,24 @@ function updateClearButtonVisibility() {
     }
 }
 
+function filterItems(e) {
+    const text = e.target.value.toLowerCase();
+    items = itemList.querySelectorAll('li');
+    items.forEach(item => {
+        const itemName = item.innerText.toLowerCase();
+        if (itemName.includes(text)) {
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        }
+    })
+}
+
 
 itemForm.addEventListener('submit', addItem)
 itemList.addEventListener('click', removeItem)
 clearBtn.addEventListener('click', clearItems)
+filter.addEventListener('input', filterItems)
 
 
 
